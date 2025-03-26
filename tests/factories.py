8caf19827e46e9fa5d001a19a3d9b9c9da9470d8
MADALA PHANINDRA <163726658+PhanindraMadala@users.noreply.github.com>
2025-03-26 17:00:00 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,1 +1,13 @@
+import factory
+from service.models import Product
+
+class ProductFactory(factory.Factory):
+    class Meta:
+        model = Product
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("word")
+    category = factory.Faker("word")
+    price = factory.Faker("random_int", min=10, max=1000)
+    availability = factory.Faker("boolean")
  
