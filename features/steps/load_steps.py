f538b3e70ee176e98a6f7059d522204747bc423a
MADALA PHANINDRA <163726658+PhanindraMadala@users.noreply.github.com>
2025-03-26 17:02:07 +0530

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -1,1 +1,9 @@
+from behave import given
+from service.models import Product
+
+@given('the database contains the following products')
+def step_impl(context):
+    for row in context.table:
+        product = Product(name=row['name'], category=row['category'], availability=row['availability'])
+        product.save()
  
